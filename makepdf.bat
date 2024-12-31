8aa84f5e43ba1cd8b26d8fbdcbefb44bb53a1cfa
xiaohang <68411935+Trikim-Zhang@users.noreply.github.com>
2024-12-31 16:18:25 +0800

    Add files via upload

--- makepdf.bat
+++ makepdf.bat
@@ -1,4 +1,4 @@
-del *.aux *.lo? *.toc *.ind *.inx *.gls *.glo *.idx *.ilg *.out *.bak *.bbl *.brf *.blg *.dvi
+del *.aux *.lo? *.toc *.ind *.inx *.gls *.glo *.idx *.ilg *.out *.bak *.bbl *.brf *.blg *.dvi data\*.aux
 xelatex main
 biber main
 xelatex main
